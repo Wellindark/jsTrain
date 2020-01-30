5b18dc10609f5b2b57a3674c782a2ced8e9d61d6
Rémi Marquilly <r.marquilly@gmail.com>
2020-01-30 13:20:50 +0100

    Created pages
    Pages for reduction, tickets and mytickets

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 import Login from './login';
 import logo from './logo.svg';
 import './App.css';
 
 export function App() {
+  const [isLogged, setIsLogged] = useState();
+
   return (
     <div className="App">
       <Router>
         <Link to="/">Accueil</Link>
         <Link to="/login">Login</Link>
+        <Link to="/tickets">Tickets</Link>
         <Switch>
           <Route path="/login">
             <Login/>
           </Route>
-          <Route path="/" component={Login}/>
+          <Route exact path={"/"}></Route>
         </Switch>
       </Router>
     </div>
---
+++ src/tickets.tsx
@@ -0,0 +1,5 @@
+import {Component} from "react";
+
+export class Tickets extends Component<any,any> {
+
+}
